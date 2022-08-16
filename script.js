7c3c55947065e110282510bc02ba38783b383a62
andyhyun <ak2572@njit.edu>
2022-08-16 11:45:06 -0400

    Allow button to change grid size

--- script.js
+++ script.js
@@ -15,7 +15,28 @@ function createGrid(n) {
   container.style.gridTemplateRows = `repeat(${n}, 1fr)`;
 }
 
+function changeGridSize() {
+  let n = prompt("Enter number of squares per side of new grid");
+  if (n === null) return;
+
+  while (isNaN(n) || n < 1 || n > 100) {
+    n = prompt("Make sure the input is a number from 1 to 100");
+    if (n === null) return;
+  }
+
+  removeGrid();
+  createGrid(parseInt(n));
+}
+
+function removeGrid() {
+  while (container.hasChildNodes()) {
+    container.firstChild.remove();
+  }
+}
+
 const container = document.querySelector(".container");
-const button = document.querySelector("button");
 
 createGrid(16);
+
+const button = document.querySelector("button");
+button.addEventListener("click", changeGridSize);
