2def30357cea017afdeb8b178cd88de8672b6702
andyhyun <ak2572@njit.edu>
2022-08-16 11:28:58 -0400

    Add grid cells

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 "use strict";
 
+function createGrid(n) {
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      const cell = document.createElement("div");
+      container.appendChild(cell);
+    }
+  }
+
+  container.style.gridTemplateColumns = `repeat(${n}, 1fr)`;
+  container.style.gridTemplateRows = `repeat(${n}, 1fr)`;
+}
+
 const container = document.querySelector(".container");
 const button = document.querySelector("button");
+
+createGrid(16);
